621a02292031e65ea82aada02c9d414fc3f7a9d7
tlin108 <tlin108@hotmail.com>
2017-01-06 22:12:05 -0500

    add createpost action creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    
     return(
-      <form>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
 
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
 
         <div className="form-group">
           <label>Content</label>
-          <textarea type="text" className="form-control" />
+          <textarea type="text" className="form-control" {...content} />
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -28,6 +31,6 @@ class PostsNew extends Component {
 }
 
 export default reduxForm({
-  form: 'PostsNew'
+  form: 'PostsNew',
   fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
