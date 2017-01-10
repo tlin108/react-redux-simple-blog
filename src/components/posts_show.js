f8c116f18fb8acce85bef0c75323bec4bb5d2411
tlin108 <tlin108@hotmail.com>
2017-01-09 21:43:37 -0500

    add delete action creator

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);  
   }
 
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id);
+  }
+
   render() {
-    return <div>Show Post {this.props.params.id}</div>;
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
+    return (
+      <div>
+        <Link to="/">Back to Index</Link>
+        <button 
+          className="btn btn-danger float-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
+      </div>
+    );
   }
 }
 
-export default connect(null, { fetchPost })(PostsShow);
+function mapStateToProps(state) {
+  return { post: state.posts.post };
+}
+
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
