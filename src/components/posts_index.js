bb69667578c2d7be05224796227b645da81cd9b8
tlin108 <tlin108@hotmail.com>
2017-01-09 20:19:18 -0500

    render list of posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -8,18 +8,37 @@ class PostsIndex extends Component {
     this.props.fetchPosts();
   }
 
+  renderPosts() {
+    return this.props.posts.map((post) => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <strong>{post.title}</strong>
+          <hr />
+          <span className="float-right">{post.categories}</span>
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <div className="text-xs-right">
+        <div className="float-right">
           <Link to="/posts/new" className="btn btn-primary">
             Add a Post
           </Link>
         </div>
-        List of blog posts
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+function mapStateToProps(state) {
+  return { posts: state.posts.all };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
